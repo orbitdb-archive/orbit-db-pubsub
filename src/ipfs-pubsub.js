bbf96fa6d2be978b203be41b1b0c1b4c6e2bd8ed
haad <haad@headbanggames.com>
2016-12-13 12:28:29 +0100

    Setup logging, handle unsubscribe, errors and ipfs.pubsub support.

--- src/ipfs-pubsub.js
+++ src/ipfs-pubsub.js
@@ -1,29 +1,36 @@
 'use strict'
 
-const logger = require('logplease').create("orbit-db.IPFSPubSub")
-
-// TODO: setup logging properly
+const logger = require('logplease').create("orbit-db.ipfs-pubsub")
+logger.setLogLevel('ERROR')
 
 class IPFSPubsub {
   constructor(ipfs) {
     this._ipfs = ipfs
     this._subscriptions = {}
+
+    if (this._ipfs.pubsub === null)
+      logger.error("The provided version of ipfs doesn't have pubsub support. Messages will not be exchanged.")
   }
 
   subscribe(hash, onMessageCallback) {
     if(!this._subscriptions[hash]) {
       this._subscriptions[hash] = { onMessage: onMessageCallback }
-      this._ipfs.pubsub.subscribe(hash, { discover: true }, (err, stream) => {
-        if (err)
-          logger.error(err)
 
-        if (stream && this._subscriptions[hash]) {
-          this._subscriptions[hash].stream = stream
-          stream.on('data', this._handleMessage.bind(this))
-          // TODO: handle end of stream
-          // stream.on('end', () => console.log("Disconnected from pubsub"))
-        }
-      })
+      if (this._ipfs.pubsub) {
+        this._ipfs.pubsub.subscribe(hash, { discover: true }, (err, stream) => {
+          if (err)
+            logger.error(err)
+
+          logger.debug(`Subscribed to '${hash}'`)
+
+          if (stream && this._subscriptions[hash]) {
+            this._subscriptions[hash].stream = stream
+            stream.on('data', this._handleMessage.bind(this))
+            // TODO: handle end of stream
+            // stream.on('end', () => console.log("Disconnected from pubsub"))
+          }
+        })
+      }
     }
   }
 
@@ -33,11 +40,12 @@ class IPFSPubsub {
         this._subscriptions[hash].stream.cancel()
 
       delete this._subscriptions[hash]
+      logger.debug(`Unsubscribed from '${hash}'`)
     }
   }
 
   publish(hash, message) {
-    if(this._subscriptions[hash])
+    if(this._subscriptions[hash] && this._ipfs.pubsub)
       this._ipfs.pubsub.publish(hash, message)
   }
 
